77862eaed81d93ce60dbbf6c819e7b9031e66d14
Neil Barot <neilbarot@gmail.com>
2015-10-26 20:03:50 +0000

    fix bugs on drivers page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/orders/new' => 'orders#new'
   post 'orders' => 'orders#create'
   get '/drivers' => 'drivers_page#index'
-  get '/drivers/accept' =>  'driver_page#accept'
+  get '/drivers/accept' =>  'drivers_page#accept'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
