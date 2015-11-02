771a1f550e5ce88d1905baf640ee602bf8e61030
Neil Barot <neilbarot@gmail.com>
2015-11-02 21:53:22 +0000

    removed test seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-neil = Users.create(name: "Neil", username: "neilbarot")
