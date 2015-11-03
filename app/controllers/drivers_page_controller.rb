68186e1bcfe968e66efee74e211019b75b32eb5c
Neil Barot <neilbarot@gmail.com>
2015-11-03 20:37:30 +0000

    implemented functional accepting of orders based on currUserID

--- app/controllers/drivers_page_controller.rb
+++ app/controllers/drivers_page_controller.rb
@@ -8,15 +8,12 @@ class DriversPageController < ApplicationController
     end
     
     def acceptOrders
-        puts "PRINTING CHECKED VALUES:"
         @workaround = params[:selectedOrders]
         if @workaround != nil
             @workaround.each do |x|
                  Order.update(x[0], :acceptedByID => Rails.application.config.currUserID)
             end
         end
-        #Take checked values and set the orders in order table with id = value so that
-        # the order's acceptedByID = Rails.application.config.currUserID
         redirect_to '/drivers/accept'
     end
 end
