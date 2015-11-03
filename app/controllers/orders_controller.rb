8b8d444eddc2464caa970877488201f2a8453cc6
Neil Barot <neilbarot@gmail.com>
2015-11-03 13:57:49 +0000

    fixed resteraunt field in orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,15 @@ class OrdersController < ApplicationController
     def create 
         @order = Order.new(order_params) 
         @order.createdByID = Rails.application.config.currUserID
+        if @order.restaurant == "http://www.starbucks.com/menu"
+            @order.restaurant = "Starbucks"
+        end
+        if @order.restaurant == "http://www.pjscoffee.com/beverages.php"
+            @order.restaurant = "PJs Coffee"
+        end
+        if @order.restaurant == "http://www.cafedumonde.com/coffee"
+            @order.restaurant = "Cafe Du Monde"
+        end
         if @order.save 
             redirect_to '/orders' 
         else 
