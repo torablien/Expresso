a508b0acbdc3a92f30137fe18a14efb2f300a625
Neil Barot <neilbarot@gmail.com>
2015-11-05 17:53:38 +0000

    displays name in header if logged user exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :setCurrUserDisplay, :except => [:create, :update, :destroy]
+  
+  def setCurrUserDisplay
+    if User.exists?(Rails.application.config.currUserID)
+        @currUserDisplay = /([^\s]+)/.match(User.find(Rails.application.config.currUserID).name)
+    else
+        @currUserDisplay = "User #" + Rails.application.config.currUserID.to_s
+    end
+  end
+  
 end
