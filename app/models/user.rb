43cb912fb3087e67c65539e57667fe2e554735f3
Neil Barot <neilbarot@gmail.com>
2015-11-05 14:26:51 +0000

    cleaned up validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
         validates :name, presence: true
-        validates :username, presence: true
-        validates_uniqueness_of :username
-        validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "is not valid" }
-        validates_uniqueness_of :email
+        validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }
+        validates :email, presence: true, uniqueness: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "is not valid" }
         validates :address, presence: true
 end
