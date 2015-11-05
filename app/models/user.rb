bfe41641cb6b72ba02d13b26d45faeb0b383f493
Neil Barot <neilbarot@gmail.com>
2015-11-05 06:28:22 +0000

    added email uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
         validates_uniqueness_of :username
         validates :name, presence: true
         validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "is not valid" }
+        validates_uniqueness_of :email
         validates :address, presence: true
 end
