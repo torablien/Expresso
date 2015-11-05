f5bf769bd9d183b88b1edba556908bd3feaff638
Neil Barot <neilbarot@gmail.com>
2015-11-05 05:54:40 +0000

    added basic user form validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+        validates :username, presence: true
+        validates :name, presence: true
+        validates :email, presence: true
+        validates :address, presence: true
 end
