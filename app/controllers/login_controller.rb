60c75cfeced62b55ae64fa05715a3a0cfeb921a0
Neil Barot <neilbarot@gmail.com>
2015-11-05 06:46:02 +0000

    added login id validation

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,8 +2,14 @@ class LoginController < ApplicationController
     def index
     end
     
+    def isInt(string)
+      true if Float(string) rescue false
+    end
+    
     def setCurrUserID
-        Rails.application.config.currUserID = params[:newCurrUserID].to_i
+        if isInt(params[:newCurrUserID])
+            Rails.application.config.currUserID = params[:newCurrUserID].to_i
+        end
         redirect_to '/login'
     end
 end
