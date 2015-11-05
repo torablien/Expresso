c3b40c98717b379a585f243d23ca09331c68ed60
Neil Barot <neilbarot@gmail.com>
2015-11-05 17:24:26 +0000

    set default scope order to id ASC

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+        default_scope{order('id ASC')}
         validates :name, presence: true
         validates :location, presence: true
         validates :time, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+        default_scope{order('id ASC')}
         validates :name, presence: true
         validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }
         validates :email, presence: true, uniqueness: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "is not valid" }
