0920d3404ae8b92d88c2ff6eef58e06c2a8426fd
Neil Barot <neilbarot@gmail.com>
2015-11-05 13:42:32 +0000

    removed test drivers model

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151101235024) do
 
-  create_table "drivers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.text     "content"
     t.text     "name"
