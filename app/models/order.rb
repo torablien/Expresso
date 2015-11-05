0eebff2a6b8a9d5889f22a2f14208eb8d3dfd1e4
Neil Barot <neilbarot@gmail.com>
2015-11-05 06:27:26 +0000

    added order validation and username uniqueness validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ActiveRecord::Base
+        validates :name, presence: true
+        validates :location, presence: true
+        validates :time, presence: true
+        validates :content, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
         validates :username, presence: true
+        validates_uniqueness_of :username
         validates :name, presence: true
         validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "is not valid" }
         validates :address, presence: true
