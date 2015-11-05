fe11a63661a148c06ac0817793515ec8147c0e19
Neil Barot <neilbarot@gmail.com>
2015-11-05 14:14:42 +0000

    created seeds for default users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+
+neil = User.create(name: "Neil Barot", username: "nbarot", email: "nbarot@tulane.edu", address: "31 McAlister Drive", isDriver: false)
+wilson = User.create(name: "Wilson Jeter", username: "wjeter", email: "wjeter@tulane.edu", address: "31 McAlister Drive", isDriver: true)
+sarah = User.create(name: "Sarah Lohmeier", username: "slohmeier", email: "slohmeie@tulane.edu", address: "31 McAlister Drive", isDriver: false)
+peter = User.create(name: "Peter Riser", username: "priser", email: "priser@tulane.edu", address: "31 McAlister Drive", isDriver: true)
+
+
+#admin = User.create(name: "Administrator", username: "admin", email: "admin@expresso-studio.com", address: "Stanley Thomas 302", isDriver: false)
