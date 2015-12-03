0b3b5a930a2506a7aa57df212115d8aa25bc0381
Neil Barot <neilbarot@gmail.com>
2015-12-03 19:23:49 +0000

    switched facebook auth to redirect to heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,523332807825030, '010bf989ae9dd0933c7b7d674e857af2'
+  provider :facebook, 1678008015746156, '4a73adab7b8ca8f3021244078cf90608'
 end
